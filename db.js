35f7b651391253fb32e1d8d61a8a6fc17229ff4d
Rodrigo Barreto <rcabarreto@gmail.com>
2019-02-19 17:38:10 -0300

    removed logging queries from sequelize config

--- db.js
+++ db.js
@@ -1,47 +1,31 @@
+const Sequelize = require('sequelize')
 
-let Sequelize = require('sequelize');
-let sequelize;
-
-let dbconfig = {
+const dbconfig = {
   logging: false,
   dialect: 'sqlite',
   storage: './data/database.sqlite',
-  dialectOptions: { 
+  dialectOptions: {
     decimalNumbers: true,
     supportBigNumbers: true,
-    bigNumberStrings: true
-  }
-};
+    bigNumberStrings: true,
+  },
+}
 
-if(process.env.NODE_ENV === 'development')
-  dbconfig.logging = console.log;
+const sequelize = new Sequelize(undefined, undefined, undefined, dbconfig)
 
-sequelize = new Sequelize(undefined, undefined, undefined, dbconfig);
-
-sequelize.authenticate().then(err => {
-  console.log('Connection has been established successfully.');
-}).catch(err => {
-  console.log('Unable to connect to the database:', err);
-});
-
-
-let db = {};
+const db = {}
 
 // IMPORTS FOR ALL MODELS
-db.user = sequelize.import(__dirname + '/models/user.js');
-db.token = sequelize.import(__dirname + '/models/token.js');
-db.sequence = sequelize.import (__dirname + '/models/sequence.js');
+db.user = sequelize.import(`${__dirname}/models/user.js`)
+db.token = sequelize.import(`${__dirname}/models/token.js`)
+db.sequence = sequelize.import(`${__dirname}/models/sequence.js`)
 
 // STARTUP
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.sequelize = sequelize
+db.Sequelize = Sequelize
 
 // RELATIONSHIPS
-db.sequence.belongsTo(db.user);
-db.user.hasMany(db.sequence);
+db.sequence.belongsTo(db.user)
+db.user.hasMany(db.sequence)
 
-// Category and navigation class self relationship
-// db.category.belongsTo(db.category, { as: 'parentCategory', foreignKey: 'parentId' });
-// db.category.hasMany(db.category, { as: 'subCategory', foreignKey: 'parentId' });
-
-module.exports = db;
+module.exports = db
